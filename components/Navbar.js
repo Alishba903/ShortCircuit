a65fa5af98a8270d66f1f5e271ae9855770b512c
Alishba <a.alishba1002@gmail.com>
2025-09-21 16:54:47 +0500

    Update Navbar component layout and styling

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='h-28 bg-purple-700 flex justify-center items-center'>
+    <nav className='h-28 bg-purple-700 flex justify-between px-3 items-center text-white '>
+      <div className="logo text-lg font-bold">
+        ShortCircuit
+      </div>
       <ul className='flex justify-center gap-4 items-center'>
         <li>Home</li>
         <li>About</li>
