3c7048b6a5a67a0f8fa21eee2a8b68116a145c7a
Alishba <a.alishba1002@gmail.com>
2025-09-21 16:49:59 +0500

    Refactor layout and Navbar components; update metadata

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,9 +2,14 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <nav className='h-28 bg-purple-700 flex justify-center items-center'>
+      <ul className='flex justify-center gap-4 items-center'>
+        <li>Home</li>
+        <li>About</li>
+        <li>Shorten</li>
+        <li>Contact Us</li>
+      </ul>
+    </nav>
   )
 }
 
