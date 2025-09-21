46e3ab84ac037a6e129bfd2a28297ac4f31808f5
Alishba <a.alishba1002@gmail.com>
2025-09-21 17:00:31 +0500

    Refactor Navbar component to use Link for navigation

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,16 +1,21 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='h-28 bg-purple-700 flex justify-between px-3 items-center text-white '>
+    <nav className='h-16 bg-purple-700 flex justify-between px-3 items-center text-white '>
       <div className="logo text-lg font-bold">
         ShortCircuit
       </div>
       <ul className='flex justify-center gap-4 items-center'>
-        <li>Home</li>
-        <li>About</li>
-        <li>Shorten</li>
-        <li>Contact Us</li>
+        <Link href="/"><li>Home</li></Link>
+        <Link href="/"><li>About</li></Link>
+        <Link href="/"><li>Shorten</li></Link>
+        <Link href="/"><li>Contact Us</li></Link>
+        <li>
+          <button>Try Now</button>
+          <button>GitHub</button>
+        </li>
       </ul>
     </nav>
   )
